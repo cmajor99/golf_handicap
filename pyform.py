5f8bc1847b2d060d6f92362019951c7ca27e5ef8
cmajor99 <43010847+cmajor99@users.noreply.github.com>
2019-11-27 11:12:01 -0500

    ficed form layout

--- pyform.py
+++ pyform.py
@@ -2,7 +2,7 @@
 import django
 
 <form action="/team_name_url/" method="post">
-    <label for="team_name">Enter name: </label>
-    <input id="team_name" type="text" name="name_field" value="Default name for team.">
+    <label for="handicap">Enter Handicap: </label>
+    <input id="handicap" type="text" name="name_field" value="ammount of handicap.">
     <input type="submit" value="OK">
 </form>
