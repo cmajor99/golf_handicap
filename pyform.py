d64b75309561c996a190950f78442ee3d9ed88a7
cmajor99 <43010847+cmajor99@users.noreply.github.com>
2019-11-27 11:21:47 -0500

    py file change

--- pyform.py
+++ pyform.py
@@ -1,8 +1,11 @@
 # this is currently just a simple layout for the form
 import django
+import golfproject.html
 
-<form action="/team_name_url/" method="post">
-    <label for="handicap">Enter Handicap: </label>
-    <input id="handicap" type="text" name="name_field" value="">
-    <input type="submit" value="OK">
-</form>
+<html>
+    <form action="/team_name_url/" method="post">
+        <label for="handicap">Enter Handicap: </label>
+        <input id="handicap" type="text" name="name_field" value="">
+        <input type="submit" value="OK">
+    </form>
+</html>
