0bf48641ef9de130811eb83cfdd3337d01c1342b
cmajor99 <43010847+cmajor99@users.noreply.github.com>
2019-11-27 11:16:04 -0500

    layout change

--- pyform.py
+++ pyform.py
@@ -3,6 +3,6 @@ import django
 
 <form action="/team_name_url/" method="post">
     <label for="handicap">Enter Handicap: </label>
-    <input id="handicap" type="text" name="name_field" value="ammount of handicap.">
+    <input id="handicap" type="text" name="name_field" value="">
     <input type="submit" value="OK">
 </form>
