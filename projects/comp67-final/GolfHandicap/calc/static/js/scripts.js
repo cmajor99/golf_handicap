a85df592f9c7b021de392e064e6454a309a78f44
cmajor99 <43010847+cmajor99@users.noreply.github.com>
2019-12-07 16:32:56 -0500

    fixed javascript

---
+++ projects/comp67-final/GolfHandicap/calc/static/js/scripts.js
@@ -0,0 +1,7 @@
+function handicap()
+        {
+            var player = document.getElementById('players').value; 
+            var course = document.getElementById('courses').value; 
+            var tee = document.getElementById('tees').value; 
+            var course = document.scores.value; 
+        }
