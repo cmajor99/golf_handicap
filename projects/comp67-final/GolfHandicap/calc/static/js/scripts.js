091f097ae450312796c2bf82463435998725fd84
cmajor99 <43010847+cmajor99@users.noreply.github.com>
2019-12-07 17:07:16 -0500

    fixed javascript

--- projects/comp67-final/GolfHandicap/calc/static/js/scripts.js
+++ projects/comp67-final/GolfHandicap/calc/static/js/scripts.js
@@ -1,7 +1,28 @@
 function handicap()
         {
-            var player = document.getElementById('players').value; 
-            var course = document.getElementById('courses').value; 
-            var tee = document.getElementById('tees').value; 
-            var course = document.scores.value; 
+            var player = document.getElementById('players').value;
+            var inputVal = "";
+            if (player) 
+            {
+                input_player = player.value;
+            }
+            console.log(player);
+            var course = document.getElementById('courses').value;
+            if (course) 
+            {
+                input_course = course.value;
+            }
+            console.log(course);
+            var tee = document.getElementById('tees').value;
+            if (tee) 
+            {
+                input_tee = tee.value;
+            }
+            console.log(tee); 
+            var score = document.getElementById('myscore').value;
+            if (score) 
+            {
+                input_score = score.value;
+            }
+            console.log(score); 
         }
